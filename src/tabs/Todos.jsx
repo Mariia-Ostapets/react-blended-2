ce8f3f399856266bc75b27a49db2d65219d40cce
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-10-06 19:24:21 +0300

    Add form

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,25 @@
-import { Text } from 'components';
+import { Text, Form } from 'components';
+import { nanoid } from 'nanoid';
+import { useEffect, useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('todos')) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = text => {
+    setTodos([...todos, { text, id: nanoid() }]);
+  };
+
+  console.log(todos);
+  return (
+    <>
+      <Form onSubmit={addTodo} />
+      <Text textAlign="center">There are no any todos ...</Text>
+    </>
+  );
 };
